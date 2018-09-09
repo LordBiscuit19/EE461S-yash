f050aa429c9a9c66e2172d32613ca3e0de6056b3
LordBiscuit19 <donaldmaze-england@utexas.edu>
2018-09-09 13:41:10 -0500

    Added basic structures for creating child processes and executing other programs.

--- main.c
+++ main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <wait.h>
 
 const int BUF_SIZE = 2000;
 const int BUF_INC = 1000;
-const char* SPACE = " ";
+
 
 void getLine(char* userIn);
 char** parseString(char* str, const char* delim);
+int exCmd(char** args);
+int lnchPrg(char** args);
+int yashCd(char** args);
+int yashHelp(char** args);
+int yashExit(char** args);
+
+const int (*SHELL_FNCTS[])(char**) = {&yashCd};
+const char* SPACE = " ";
+const char* SHELL_CMDS[] = { "cd", "help", "exit"};
+const int NUM_SHELL_CMDS = 3;
 
 
 int main() {
@@ -22,15 +34,23 @@ int main() {
             printf("%s\n", *(parsedIn + i));
             i++;
         }
-        if (strcmp(parsedIn[0],"exit") == 0)
-            break;
+        if (exCmd(parsedIn) == 42){
+            i = 0;
+            while (*(parsedIn + i) != NULL){
+                free(*(parsedIn + i));
+                i++;
+            }
+            free(userIn);
+            free(parsedIn);
+            exit (0);
+        }
+        i = 0;
+        while (*(parsedIn + i) != NULL){
+            free(*(parsedIn + i));
+            i++;
+        }
     }
-    free(userIn);
-    for (int i = 0; i < sizeof(parsedIn)/sizeof(char**); i++){
-        free(*(parsedIn + i));
-    }
-    free(parsedIn);
-    exit (0);
+
 }
 
 
@@ -81,4 +101,43 @@ char** parseString(char* str, const char* delim){
     }
     *(parsed + parsedPtr) = NULL;
     return parsed;
+}
+
+
+//executes the user command. First it checks to see if the command is one of the built in commands in the shell
+int exCmd(char** args){
+    for (int i = 0; i < NUM_SHELL_CMDS; i++){
+        if (strcmp(args[0], SHELL_CMDS[i]) == 0){
+            return (*SHELL_FNCTS[i])(args);
+        }
+    }
+    return lnchPrg(args);
+}
+
+
+//executes all user commands that require an external program
+int lnchPrg(char** args){
+    pid_t pid;
+    pid = fork();
+    int* waitStat;
+    if (pid == 0){
+        execvp(args[0], args);
+        printf("execvp error in lnchPrg");
+        return(-1);
+    }
+    if (pid == -1){
+        printf("error forking from lnchPrg");
+        return(-2);
+    }
+    waitpid(pid, &waitStat, 0);
+    return (0);
+}
+
+int yashCd(char** args){
+    printf("testing cd");
+    return 1;
+}
+
+int yashExit(char** args){
+    return 42;
 }
