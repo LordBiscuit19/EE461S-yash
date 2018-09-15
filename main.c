77d399fdd1beb1f0e1ec24970dd9293238fe84ae
LordBiscuit19 <donaldmaze-england@utexas.edu>
2018-09-15 16:45:47 -0500

    Added basic file redirection and started to implement pipeing

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <wait.h>
+#include <fcntl.h>
 
 const int BUF_SIZE = 2000;
 const int BUF_INC = 1000;
@@ -10,31 +11,58 @@ const int BUF_INC = 1000;
 
 void getLine(char* userIn);
 char** parseString(char* str, const char* delim);
-int exCmd(char** args);
-int lnchPrg(char** args);
-int yashCd(char** args);
+int exCmd1(char** args);
+int exCmd2(char** args1, char** arg2);
+int lnchPrg1(char** args);
+int lnchPrg2 (char** args1, char** args2);
 int yashHelp(char** args);
 int yashExit(char** args);
+int lookForPipe(char** args);
+int lookForRedir(char** args, int* returns);
 
-const int (*SHELL_FNCTS[])(char**) = {&yashCd};
+int (*SHELL_FNCTS[])(char**) = {&yashExit};
 const char* SPACE = " ";
-const char* SHELL_CMDS[] = { "cd", "help", "exit"};
-const int NUM_SHELL_CMDS = 3;
+const char* SHELL_CMDS[] = {"exit"};
+const int NUM_SHELL_CMDS = 1;
 
 
 int main() {
     char* userIn = malloc(sizeof(char)*BUF_SIZE);
     char** parsedIn;
+    char** parsedIn2 = NULL;
     while (1){
-        printf("$ ");
+        printf("# ");
         getLine(userIn);
         parsedIn = parseString(userIn, SPACE);
         int i = 0;
+        /*
         while (*(parsedIn + i) != NULL){
             printf("%s\n", *(parsedIn + i));
             i++;
         }
-        if (exCmd(parsedIn) == 42){
+         */
+        int pipeOffset = lookForPipe(parsedIn);
+        if (pipeOffset != -1){
+            parsedIn2 = parsedIn + pipeOffset + 1;    //parsedIn2 is the command after the pipe
+            *(parsedIn + pipeOffset) = NULL; //split the user input string into the command before and after the pipe
+            if (exCmd2(parsedIn, parsedIn2) == 42) {
+                i = 0;
+                while (*(parsedIn + i) != NULL) {
+                    free(*(parsedIn + i));
+                    i++;
+                }
+                i = 0;
+                while (*(parsedIn2 + i) != NULL) {
+                    free(*(parsedIn2 + i));
+                    i++;
+                }
+                free(userIn);
+                free(parsedIn);
+                free(parsedIn2);
+                exit(0);
+            }
+        }
+        else if (exCmd1(parsedIn) == 42){
             i = 0;
             while (*(parsedIn + i) != NULL){
                 free(*(parsedIn + i));
@@ -48,6 +76,15 @@ int main() {
         while (*(parsedIn + i) != NULL){
             free(*(parsedIn + i));
             i++;
+        }
+        free(parsedIn);
+        if (parsedIn2 != NULL) {
+            while (*(parsedIn2 + i) != NULL) {
+                free(*(parsedIn2 + i));
+                i++;
+            }
+            free(parsedIn2);
+            parsedIn2 = NULL;
         }
     }
 
@@ -104,39 +141,172 @@ char** parseString(char* str, const char* delim){
 }
 
 
+int lookForPipe (char** args){
+    int i = 0;
+    while(*(args + i) != NULL){
+        if (strcmp(*(args+i), "|") == 0) {
+            return (i); //if a pipe is found return it's offset
+        }
+        i++;
+    }
+    return (-1); //-1 will be the signal that a pipe was not found.
+}
+
+//***************************************
+//Returns: -1 if no redirect found, 1 if redirect is found.
+//searches args for a redirect symbol. The returns variable is used to point to the return values of the
+//function. lookForRedir places the offset of the redirection symbol in args in the first index of the
+//returns array. It also places 1 in the second index of returns to signify output redirection and -1 to
+//signify input redirection.
+int lookForRedir(char** args, int* returns){
+    //******
+    //to-do: handle the case of multiple file redirect symbols (need an index for each symbol)
+    int i = 0;
+    while (*(args + i) != NULL){
+        if (strcmp(*(args+i), ">") == 0){
+            *(returns + 0) = i; //puts the offset of the redirection symbol in the first spot of returns
+            *(returns + 1) = 1; //puts 1 in the second spot of returns to signify output redirection
+            return 1; //returns 1 if it found a redirect
+        }
+        if (strcmp(*(args+i), "<") == 0){
+            *(returns + 0) = i; //puts the offset of the redirection symbol in the first spot of returns
+            *(returns + 2) = 1; //puts 1 in the third spot of returns to signify input redirection
+            return 1; //returns 1 if it found a redirect
+        }
+        if (strcmp(*(args+i), "2>") == 0){
+            *(returns + 0) = i; //puts the offset of the redirection symbol in the first spot of returns
+            *(returns + 3) = 1; //puts 1 in the fourth spot of returns to signify error redirection
+            return 1; //returns 1 if it found a redirect
+        }
+        i++;
+    }
+    return (-1); //returns -1 if it didn't find a redirect
+}
+
+
 //executes the user command. First it checks to see if the command is one of the built in commands in the shell
-int exCmd(char** args){
+int exCmd1(char** args){
     for (int i = 0; i < NUM_SHELL_CMDS; i++){
         if (strcmp(args[0], SHELL_CMDS[i]) == 0){
             return (*SHELL_FNCTS[i])(args);
         }
     }
-    return lnchPrg(args);
+    return lnchPrg1(args);
+}
+
+
+int exCmd2(char** args1, char** args2){
+    for (int i = 0; i < NUM_SHELL_CMDS; i++){
+        if (strcmp(args1[0], SHELL_CMDS[i]) == 0){
+            return (*SHELL_FNCTS[i])(args1);
+        }
+    }
+    for (int i = 0; i < NUM_SHELL_CMDS; i++){
+        if (strcmp(args2[0], SHELL_CMDS[i]) == 0){
+            return (*SHELL_FNCTS[i])(args2);
+        }
+    }
+    return lnchPrg2(args1, args2);
 }
 
 
 //executes all user commands that require an external program
-int lnchPrg(char** args){
+int lnchPrg1(char** args){
     pid_t pid;
-    pid = fork();
     int* waitStat;
-    if (pid == 0){
-        execvp(args[0], args);
-        printf("execvp error in lnchPrg");
-        return(-1);
+    pid = fork();
+
+
+    if (pid == 0){ //child
+        int* redirOffsetAndType = malloc(sizeof(int) * 4);
+        for (int i = 0; i < 4; i++){    //initialize array of data used for file redirection detection
+            *(redirOffsetAndType + i) = -1;
+        }
+
+        if(lookForRedir(args, redirOffsetAndType) == 1){    //there is a redirect
+            //to-do: handle the case of multiple redirect symbols
+            int redirSymbolOffset = redirOffsetAndType[0];
+            printf ("filename : %s\n", args[redirSymbolOffset + 1]);
+            char* fileName = args[redirSymbolOffset + 1];
+            *(args + redirSymbolOffset) = NULL; //cut off the file name from the args list so we can send it to execvp
+            //printf("args[0]: %s\n", args[0]);
+
+            if(redirOffsetAndType[1] == 1){
+                int fd = open(fileName, O_WRONLY|O_TRUNC|O_CREAT, S_IRWXU);
+                //printf ("fd: %d\n", fd);
+                dup2(fd, STDOUT_FILENO);
+            }
+
+            if (redirOffsetAndType[2] == 1){
+                int fd = open(fileName, O_RDONLY);
+                //printf ("fd: %d\n", fd);
+                dup2(fd, STDIN_FILENO);
+            }
+
+            if(redirOffsetAndType[3] == 1){
+                int fd = open(fileName, O_WRONLY|O_TRUNC|O_CREAT, S_IRWXU);
+                //printf ("fd: %d\n", fd);
+                dup2(fd, STDERR_FILENO);
+            }
+
+            free(redirOffsetAndType);
+            execvp(args[0], args);
+            printf("execvp error in lnchPrg1");
+            return(-1);
+        }
+
+        else { //there is no redirect
+            free(redirOffsetAndType);
+            execvp(args[0], args);
+            printf("execvo error in lnchPrg1");
+            return (-1);
+        }
     }
-    if (pid == -1){
-        printf("error forking from lnchPrg");
+
+
+    else if (pid == -1){
+        printf("error forking from lnchPrg1");
         return(-2);
     }
-    waitpid(pid, &waitStat, 0);
-    return (0);
+
+
+    else { //parent
+        setpgid(pid, pid);
+        waitpid(pid, waitStat, 0);
+        return (0);
+    }
 }
 
-int yashCd(char** args){
-    printf("testing cd");
-    return 1;
+
+int lnchPrg2(char** args1, char** args2){
+    pid_t pid1, pid2;
+    int pipeFd[2]; //pipeFd[0] read end pipeFd[1] write end
+    pipe(pipeFd);
+    int* waitStat;
+    pid1 = fork();
+    if (pid1 != 0){ //parent
+        pid2 = fork();
+        if (pid2 != 0){//parent
+            close(pipeFd[0]);
+            close(pipeFd[1]);
+            setpgid(pid1, pid1);
+            setpgid(pid2, pid1);
+            waitpid(pid2, waitStat, 0);
+        }
+        else{ //child 2 read end of pipe
+            close(pipeFd[1]);
+            dup2(pipeFd[0], STDIN_FILENO);
+            execvp(args2[0], args2);
+        }
+    }
+    else{ //child 1 write end of pipe
+        close(pipeFd[0]);
+        dup2(pipeFd[1], STDOUT_FILENO);
+        execvp(args1[0], args1);
+    }
+
 }
+
 
 int yashExit(char** args){
     return 42;
